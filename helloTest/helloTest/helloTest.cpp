074f5d3e29865bc885698a92ef7a932f0f8e84b0
dusk96 <dsk_163mail@163.com>
2021-06-03 20:43:56 +0800

    commit C++ project

---
+++ helloTest/helloTest/helloTest.cpp
@@ -0,0 +1,14 @@
+// helloTest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "iostream"
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	system("pause");
+	return 0;
+}
+
